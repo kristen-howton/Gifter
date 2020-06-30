fe4ef07e7c6ac9909910ded81fde059351c8b1b7
Kristen Howton <kristen.howton@gmail.com>
2020-06-30 14:21:47 -0500

    intial commit

---
+++ Gifter/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Gifter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gifter.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //These are specific to our project. We pass in the model classes
+        public DbSet<UserProfile> UserProfile { get; set; }
+        public DbSet<Post> Post { get; set; }
+    }
+}
