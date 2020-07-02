3804a0c979d817f7b1266b7e5e5904f441bb7625
Kristen Howton <kristen.howton@gmail.com>
2020-07-01 22:55:38 -0500

    comment section built out

---
+++ Gifter/Controllers/CommentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gifter.Data;
+using Gifter.Repositories;
+using Gifter.Models;
+using System.Collections.Generic;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly CommentRepository _commentRepository;
+
+        //using context instead of config
+        public CommentController(ApplicationDbContext context)
+        {
+            _commentRepository = new CommentRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_commentRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var comment = _commentRepository.GetById(id);
+            if (comment == null)
+
+            {
+                return NotFound();
+            }
+            return Ok(comment);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+
+            _commentRepository.Update(comment);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _commentRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Gifter/Repositories/CommentRepository.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public class CommentRepository
+    {
+        //saving an instance of our app db context
+        private readonly ApplicationDbContext _context;
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Comment> GetAll()
+        {
+            return _context.Comment
+                            .Include(c => c.Post)
+                            .ToList();
+        }
+        public Comment GetById(int id)
+        {
+            return _context.Comment
+                            .Include(c => c.Post)
+                            .FirstOrDefault(c => c.Id == id);
+        }
+        public void Add(Comment comment)
+        {
+            _context.Add(comment);
+            _context.SaveChanges();
+        }
+
+        public void Update(Comment comment)
+        {
+            _context.Entry(comment).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var comment = GetById(id);
+            _context.Comment.Remove(comment);
+            _context.SaveChanges();
+        }
+    
+        public List<Comment> GetByUserPostId(int id)
+        {
+            return _context.Comment.Include(c => c.Post)
+                            .Where(c => c.Id == id)
+                            .OrderBy(c => c.Message)
+                            .ToList();
+        }
+    }
+}
